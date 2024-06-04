90055cbad74ea2a715a14376cff8591408b7bfbd
OmarDev10 <serio@northlands.edu.ar>
2024-06-04 11:19:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function Game1()
+{
+    window.location.replace("Game1.html");
+    console.log("Stating game1")
+}
+function Home()
+{
+    window.location.replace("index.html");
+}
+function Game2()
+{
+    window.location.replace("Game2.html")
+    console.log("Starting game2");
+}
+function Game3()
+{
+    window.location.replace("Game3.html")
+    console.log("Starting game3");
+}
